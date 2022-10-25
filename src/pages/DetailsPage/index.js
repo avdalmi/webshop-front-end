b7856187229add68f1c6d5f8fc76647027fa4036
AdneCode <super-gijs@hotmail.com>
2022-10-25 12:06:02 +0200

    Implemented retrieval of productData in DetailsPage

--- src/pages/DetailsPage/index.js
+++ src/pages/DetailsPage/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useParams } from "react-router-dom";
 
 function DetailsPage() {
+    const [product, setProduct] = useState(null)
+    const params = useParams()
+    const productId = params.id
+    console.log(productId)
+    const getProductById = async () =>{
+    const productData = await axios.get(`http://localhost:4000/products/${productId}`)
+    return productData
+}
+const productData = getProductById()
+console.log(productData)
     return (
-        <div> DetailsPage</div>
+        <div></div>
     );
 }
 
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./styles.css";
-import Banner from '/Users/Akiko_1/Desktop/frontend/src/components/Banner';
+import Banner from '../../components/Banner';
 
 
 function Homepage() {
